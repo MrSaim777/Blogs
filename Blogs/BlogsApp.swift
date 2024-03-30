e977f6f9e9fa6602430181e5084dfb871e8e304f
usamasultan <usamasultan@rev9solutions.com>
2024-03-30 08:31:07 +0500

    Initial Commit

---
+++ Blogs/BlogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogsApp.swift
+//  Blogs
+//
+//  Created by Usama Sultan on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BlogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
